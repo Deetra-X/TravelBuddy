36bf8e1e9112d28b13b3be2336f1a80d7fc07f87
Deetra-X <cobsccomp242p-025@student.nibm.lk>
2026-04-09 15:00:41 +0530

    Initial commit

---
+++ TravelBuddy/TravelBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelBuddyApp.swift
+//  TravelBuddy
+//
+//  Created by Bsccomp242p-025 on 2026-04-09.
+//
+
+import SwiftUI
+
+@main
+struct TravelBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
